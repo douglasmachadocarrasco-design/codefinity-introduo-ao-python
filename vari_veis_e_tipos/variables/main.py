67cad7aa501d09d88ac4010e1bd13ceda19f0a5a
douglasmachadocarrasco-design <>
2025-11-09 13:31:47 +0000

    Variables

---
+++ vari_veis_e_tipos/variables/main.py
@@ -0,0 +1,11 @@
+#Tarefa dos ovos
+
+item_name = "Eggs"
+item_price = 3.99
+item_quantity = 120
+in_stock = True
+
+print ("Item: ", item_name)
+print ("Price: ", item_price)
+print ("Quantity:  ", item_quantity)
+print ("In stock: ", in_stock)
